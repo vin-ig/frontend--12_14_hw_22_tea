819b7ad13b352f3270606556f43161b08450c68a
Vinokurov Igor <vin-ig@yandex.ru>
2025-10-07 18:37:16 +0300

    Приложение декомпозировано на компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { ProductComponent } from './components/pages/product/product.component';
+import { ProductsComponent } from './components/pages/products/products.component';
+import { OrderComponent } from './components/pages/order/order.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { ProductCardComponent } from './components/common/product-card/product-card.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [
         AppComponent,
+        MainComponent,
+        ProductComponent,
+        ProductsComponent,
+        OrderComponent,
+        HeaderComponent,
+        FooterComponent,
+        ProductCardComponent,
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
